93c276d14644b89db767e43dd84e814d7c69045b
Rahul Sharma <rahul@rahulsharma.uk>
2019-09-28 12:48:39 +0100

    started workin on pickup manager and updated scene objects slightly

--- Assets/PickupManager.cs
+++ Assets/PickupManager.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private GameObject[] pickups;
+	private GameObject currentTarget;
+	private bool collectedItem;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		pickups = GameObject.FindGameObjectsWithTag("Pickup");
+		currentTarget = null;
+		collectedItem = true;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (collectedItem)
+		{
+			StartCoroutine(PickItem());
+		}
+		else { 
+			
+		}
+	}
+
+	IEnumerator PickItem() {
+		collectedItem = false;
+		yield return new WaitForSeconds(5f);
+		int i = Random.Range(0, pickups.Length - 1);
+		currentTarget = pickups[i];
+		Debug.Log("Current Target: " + currentTarget.name);
+		yield return null;
+	}
 }
